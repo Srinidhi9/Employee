ff114c3f2c21deb1712802d4e1d063618c5f9eac
venkateshkuthadi <venkateshkuthadi.vtalent@gmail.com>
2024-10-24 12:44:41 +0530

    Bean Changes

---
+++ EmployeeData/src/main/java/com/Employee/Bean/EmployeeData.java
@@ -0,0 +1,59 @@
+package com.Employee.Bean;
+
+public class EmployeeData {
+	private String EmployeeId;
+	private String FristName;
+	private String LastName;
+	private String DOB;
+	private String Email;
+	private String Phone;
+	private String Gender;
+	private String EmployeeType;
+	private String Education;
+	private String Skills;
+	
+	public String getEmployeeId() {
+		return EmployeeId;
+	}
+	public void setEmployeeId(String employeeId) {
+		EmployeeId = employeeId;
+	}
+	public String getFristName() {
+		return FristName;
+	}
+	public void setFristName(String fristName) {
+		FristName = fristName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getPhone() {
+		return Phone;
+	}
+	public void setPhone(String phone) {
+		Phone = phone;
+	}
+	public String getGender() {
+		return Gender;
+	}
+	public void setGender(String gender) {
+		Gender = gender;
+	}
+	public String getEmployeeType() {
+		return EmployeeType;
+	}
+	public void setEmployeeType(String employeeType) {
+		EmployeeType = employeeType;
+	}
+	
+	
+}
