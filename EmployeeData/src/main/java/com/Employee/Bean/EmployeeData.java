392e726c6b3536b4d8d99047a3ad0c08ecda9d65
Zoro072002 <lingojuabhiram@gmail.com>
2024-10-25 11:17:27 +0530

    Dao Changes and Minor fix in Bean

--- EmployeeData/src/main/java/com/Employee/Bean/EmployeeData.java
+++ EmployeeData/src/main/java/com/Employee/Bean/EmployeeData.java
@@ -2,7 +2,7 @@ package com.Employee.Bean;
 
 public class EmployeeData {
 	private String EmployeeId;
-	private String FristName;
+	private String FirstName;
 	private String LastName;
 	private String DOB;
 	private String Email;
@@ -12,48 +12,29 @@ public class EmployeeData {
 	private String Education;
 	private String Skills;
 	private String Experience;
-	
-	public String getDOB() {
-		return DOB;
-	}
-	public void setDOB(String dOB) {
-		DOB = dOB;
-	}
-	public String getEducation() {
-		return Education;
-	}
-	public void setEducation(String education) {
-		Education = education;
-	}
-	public String getSkills() {
-		return Skills;
-	}
-	public void setSkills(String skills) {
-		Skills = skills;
-	}
-	public String getExperience() {
-		return Experience;
-	}
-	public void setExperience(String experience) {
-		Experience = experience;
-	}
 	public String getEmployeeId() {
 		return EmployeeId;
 	}
 	public void setEmployeeId(String employeeId) {
 		EmployeeId = employeeId;
 	}
-	public String getFristName() {
-		return FristName;
+	public String getFirstName() {
+		return FirstName;
 	}
-	public void setFristName(String fristName) {
-		FristName = fristName;
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
 	}
 	public String getLastName() {
 		return LastName;
 	}
 	public void setLastName(String lastName) {
 		LastName = lastName;
+	}
+	public String getDOB() {
+		return DOB;
+	}
+	public void setDOB(String dOB) {
+		DOB = dOB;
 	}
 	public String getEmail() {
 		return Email;
@@ -79,6 +60,25 @@ public class EmployeeData {
 	public void setEmployeeType(String employeeType) {
 		EmployeeType = employeeType;
 	}
-	
-	
+	public String getEducation() {
+		return Education;
+	}
+	public void setEducation(String education) {
+		Education = education;
+	}
+	public String getSkills() {
+		return Skills;
+	}
+	public void setSkills(String skills) {
+		Skills = skills;
+	}
+	public String getExperience() {
+		return Experience;
+	}
+	public void setExperience(String experience) {
+		Experience = experience;
+	}
 }
+	
+	
+	
---
+++ EmployeeData/src/main/java/com/Employee/Dao/Dao.java
@@ -0,0 +1,76 @@
+package com.Employee.Dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.Employee.Bean.EmployeeData;
+
+
+public class Dao {
+		Connection  connection;
+		PreparedStatement preparedstatement;
+		ResultSet resultset;
+
+		public Dao() {
+			connection = My_SQL_connections.getInstance();
+		}
+
+		public int insertDataWithpreparedStatement(EmployeeData s) {
+			int result = 0;
+			String query = "insert into student_details values(?,?,?,?,?,?,?,?,?,?,?)";
+			try {
+				preparedstatement = connection.prepareStatement(query);
+				preparedstatement.setString(1, s.getEmployeeId());
+				preparedstatement.setString(2, s.getFirstName());
+				preparedstatement.setString(3, s.getLastName());
+				preparedstatement.setString(4, s.getDOB());
+				preparedstatement.setString(5, s.getEmail());
+				preparedstatement.setString(6, s.getPhone());
+				preparedstatement.setString(7, s.getGender());
+				preparedstatement.setString(8, s.getEmployeeType());
+				preparedstatement.setString(9, s.getEducation());
+				preparedstatement.setString(10, s.getSkills());
+				preparedstatement.setString(11, s.getExperience());
+				result = preparedstatement.executeUpdate();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			return result;
+		}
+
+			public int deleteDataWithpreparedStatement(EmployeeData s) {
+				int result1 = 0;
+				String query = "delete from EmployeeData where EmployeeId=?";
+				try {
+					preparedstatement = connection.prepareStatement(query);
+					preparedstatement.setString(1, s.getEmployeeId());
+					result1 = preparedstatement.executeUpdate();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				return result1;
+			
+			}		
+
+			public int updateData(EmployeeData s,String field, String value,String EmployeeID) {
+				int result1 = 0;
+				String query = "update EmployeeData set "+field+"="+value+" where EmployeeID="+EmployeeID;
+				try {
+					preparedstatement = connection.prepareStatement(query);
+					result1 = preparedstatement.executeUpdate();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				return result1;
+
+
+
+			}
+		}
+		
+
+
